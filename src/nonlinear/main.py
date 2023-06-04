5541ce3b4e5fff11a67e42dd6bafd9ffc505d81e
wyyaa123 <863114354@qq.com>
2023-06-04 16:26:55 +0800

    ros调试

--- src/nonlinear/main.py
+++ src/nonlinear/main.py
@@ -1,153 +1,217 @@
-"""
 # -*- coding: utf-8 -*-
-# @Time : 2023-05-23 14:21
-# @Author : orCate
-"""
+import sys
+import time
+sys.path.append("/home/wyyaa123/IMUdeblur/")
+import rospy
+import lib.Image as _Image
+import lib.PSF as _PSF
+import lib.frame as _Frame
 import numpy as np
 import cv2 as cv
-import Image
-import time
+from cv_bridge import CvBridge
+# import message_filters
+from sensor_msgs.msg import Image
+from nav_msgs.msg import Odometry
 
+def quaternion_to_Rotation(qu: np.ndarray) -> np.ndarray:
+    R = np.zeros((3, 3), dtype=float)
+    R[0, 0] = qu[0] ** 2 + qu[1] ** 2 - qu[2] ** 2 - qu[3] ** 2
+    R[0, 1] = 2 * (qu[1] * qu[2] - qu[0] * qu[3])
+    R[0, 2] = 2 * (qu[1] * qu[3] + qu[0] * qu[2])
+    R[1, 0] = 2 * (qu[1] * qu[2] + qu[0] * qu[3])
+    R[1, 1] = qu[0]**2 - qu[1] ** 2 + qu[2] ** 2 - qu[3] ** 2
+    R[1, 2] = 2 * (qu[2] * qu[3] - qu[0] * qu[1])
+    R[2, 0] = 2 * (qu[1] * qu[3] - qu[0] * qu[2])
+    R[2, 1] = 2 * (qu[2] * qu[3] + qu[0] * qu[1])
+    R[2, 2] = qu[0] ** 2 - qu[1] ** 2 - qu[2] ** 2 + qu[3] ** 2
+    
+    return R
 
-if __name__ == '__main__':
-    """
-    # width = 320
-    # height = 160
+def Roation_to_Euler(R: np.ndarray) -> np.ndarray:
+    # 计算欧拉角
+    # Pitch (绕x轴旋转) θ
+    pitch = np.arcsin(R[1, 2])
+    # heading (绕z轴旋转) ψ
+    heading = np.arctan2(R[1, 0], R[1, 1])
+    # Roll (绕y轴旋转) r
+    roll = np.arctan2(-R[0, 2], R[2, 2])
+    
+    pitch_deg = np.degrees(pitch)
+    heading_deg = np.degrees(heading)
+    roll_deg = np.degrees(roll)
+    
+    return np.array([heading_deg, pitch_deg, roll_deg])
 
-    # x_coors = np.linspace(0, width - 1, width) # 此处注释的代码的运算结果是一个矩阵->维度{3 x (图像高 . 图像宽)}, 
-    矩阵是由图像像素点坐标[X, Y, 1]组成,所以可以直接用单应矩阵(3 x 3)乘以他得到像素点变化后的像素坐标
-    # print("x_coors is :", x_coors)
-    # y_coors = np.linspace(0, height - 1, height)
-    # print("y_coors is :", y_coors)
-    # indices = np.meshgrid(x_coors, y_coors)
-    # print("indices is :", indices)
-    # indices = np.stack(indices)
-    # print("indices is :", indices)
-    # indices = indices.T
-    # indices = indices.reshape((-1, 2))
-    # print("indices is :", indices)
-    # indices = indices.T
-    # indices = np.vstack((indices, np.ones(height * width)))
-    # print("indices is :", indices)
-    # indices /= indices[2, :]
-    # indices = indices.astype('int')
-    # print("indices is :", indices)
+def deblur_byIMU(Rij: np.ndarray, tij: np.ndarray, raw_image: cv.Mat, depth_image: cv.Mat, K: np.ndarray, N: int = 16, overlap: int = 16) -> cv.Mat:
+    image_blocks = _Image.segment_nimage(raw_image, N, overlap, False)
+    block_psfs = _Image.calcu_each_block_psf(False, image_blocks, depth_image, N, K, Rij, tij, overlap)
+    
+    laplacian = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
+    for row in image_blocks.shape[0]:
+        for col in image_blocks.shape[1]:
+            image_blocks[row, col] = _Image.cls_filter(image_blocks[row, col], block_psfs[row, col], laplacian, 0.001)
+    
+    deblur_image = _Image.nimage_block_merge(image_blocks, N, overlap, False)
+    return deblur_image
 
-    K = np.array([[726.28741455078, 0, 354.6496887207],# 此处注释是用来计算不同的四个像素点在相同单应矩阵时的像素变化
-                  [0, 726.28741455078, 186.46566772461],
+def img0_callback(img0: Image):
+    global t_i, R_i, depths, frames, img0_pub, depth_image
+    
+    frame_flag: bool = False
+    depth_flag: bool =  False
+    
+    R_j = np.eye(3, dtype=float)
+    t_j = [0, 0, 0]
+    
+    bridge = CvBridge()
+    cv_image = bridge.imgmsg_to_cv2(img0, "mono8")
+    
+    # print("img0的时间戳为", img0.header.stamp.to_sec())
+    
+    for frame in frames: #匹配帧
+        # print("frame的时间戳为", frame.timestamp)
+        if np.abs(img0.header.stamp.to_sec() - frame.timestamp) < 0.03:
+            # print("对应帧匹配成功!")
+            frame_flag = True
+            R_j = frame.Rj
+            t_j = frame.tj
+            
+            print(frame.timestamp)
+            np_frame = np.array(frames)      
+            temp_frame = np_frame == frame
+            # print(temp_depth)
+            indices = np.where(temp_frame == True)
+            # print(indices)
+            frames[:int(indices[0])] = []
+            break
+    
+    for depth in depths: #匹配深度图
+        # print("深度图的时间戳为", depth.header.stamp.to_sec())
+        if np.abs(img0.header.stamp.to_sec() - depth.header.stamp.to_sec()) < 0.03: # 这里如果没有找到匹配的深度图会使用上一帧的深度图
+            # print("对应深度图匹配成功!")
+            
+            # 这一部分这么写是因为只有np数组判断后的结果仍然是一个布尔类型的数组, 列表里哪个元素符合条件, 然后把他之前的所有数据清空
+            # print("深度图的长度为", np.size(depths))
+            np_depth = np.array(depths)      
+            temp_depth = np_depth == depth
+            # print(temp_depth)
+            indices = np.where(temp_depth == True)
+            # print(indices)
+            depths[:int(indices[0])] = []
+            
+            depth_flag = True
+            depth_image = bridge.imgmsg_to_cv2(depth, "passthrough") #16UC1
+            depth_image.astype(np.uint16) #转为无符号16位
+            # print(depth_image.shape)
+            break
+        
+    if not depth_flag: # 直接全部清空
+        depths = []
+        
+    # print("the size of depths is", np.size(depths))
+        
+    if frame_flag: #得到深度图和匹配帧
+        print("OK!")
+        
+        Rij  = R_j * np.linalg.inv(R_i)
+        tij = t_j - Rij @ t_i
+        # deblur_image = None
+        # deblur_image = deblur_byIMU(Rij, tij, cv_image, depth_image, K, 16, 16)  
+        # deblur_image_msg = bridge.cv2_to_imgmsg(deblur_image, "passthrough")
+        # img0_pub.publish(deblur_image_msg)
+        # pass
+        print("Rij is", Rij)
+        print("Ri is", R_i)
+        print("Rj is", R_j)
+        print("tij is", tij)
+        print("ti is", t_i)
+        print("tj is", t_j)
+        depth_msg = bridge.cv2_to_imgmsg(depth_image, "passthrough")
+        img0_pub.publish(depth_msg)
+    else:
+        print("game over!")
+        # pass
+    
+    R_i = R_j
+    t_i = t_j
+   
+    left_imgs.append(img0)
+    if np.size(left_imgs) > 2:
+        left_imgs.pop(0)
+    
+    # pass
+    # print("img0 callback!")
+
+def img1_callback(img1: Image):    
+    global img1_pub
+    
+    right_imgs.append(img1)
+    if np.size(right_imgs) > 2:
+        right_imgs.pop(0)
+        
+    img1_pub.publish(img1)
+    # pass
+    # print("img1 callback!")
+
+def depth_callback(depth: Image):
+           
+    depths.append(depth)
+    if np.size(depths) > 100:
+        depths.pop(0)
+    # pass
+    # print("depth callback!") 
+    
+    
+def imu_callback(pose_j: Odometry):
+    # global R_i, t_i
+    # print("imu callback!")
+    global frames
+    R_j = quaternion_to_Rotation(np.array([pose_j.pose.pose.orientation.w, pose_j.pose.pose.orientation.x, 
+                                           pose_j.pose.pose.orientation.y, pose_j.pose.pose.orientation.z]))
+    
+    # print("R_j is", R_j)
+    
+    t_j = np.array([pose_j.pose.pose.position.x, pose_j.pose.pose.position.y, pose_j.pose.pose.position.z])
+    
+    frame_j = _Frame.Frame(pose_j.header.stamp.to_sec(), Rj=R_j, tj=t_j)
+    
+    frames.append(frame_j)
+    if np.size(frames) > 1e2:
+        frames.pop(0)
+    # print("t_j is", t_j)
+    
+    # Rij = R_j @ np.linalg.inv(R_i)
+    # tij = t_j - Rij @ t_i
+    
+    # R_i = R_j
+    # t_i = t_j
+    
+    # print("Rij is ", Rij)
+    # print("tij is", tij)
+    
+if __name__ == "__main__":          
+    rospy.init_node("deblur_node") #初始化ros节点  
+
+    # pose_i = Odometry()
+    R_i = np.eye(3, dtype=float)
+    t_i = np.array([0, 0, 0], dtype=float)
+    depth_image = np.ones((400, 640), dtype=np.uint16)
+    
+    frames = []
+    depths = []
+    left_imgs = []
+    right_imgs = []
+    
+    K = np.array([[407.76044960599006, 0, 309.58075680219565],
+                  [0, 406.5997999128962, 193.42444301176783],
                   [0, 0, 1]])
     
-    R = np.array([[1, 0, 0],
-                  [0, 1, 0],
-                  [0, 0, 1]])
+    img0_sub = rospy.Subscriber("/stereo_inertial_publisher/left/image_rect", Image, img0_callback)
+    img1_sub = rospy.Subscriber("/stereo_inertial_publisher/right/image_rect", Image, img1_callback)
+    depth_sub = rospy.Subscriber("/stereo_inertial_publisher/stereo/depth", Image, depth_callback)
+    imu_propagate_sub = rospy.Subscriber("/vins_estimator/imu_propagate", Odometry, imu_callback)
     
-    t = np.array([0.1, 0.1, 0]) 
-    N = np.array([0, 0, 1])
-    d = 10
-
-    H = K @ (R + (1 / d) * np.outer(t.T, N)) @ np.linalg.inv(K)
-
-    # print("H is", H)
-    #
-    # leftup = np.array([0, 0, 1])
-    # leftdown = np.array([0, 159, 1])
-    # rightup = np.array([319, 0, 1])
-    # rightdown = np.array([319, 159, 1])
-    #
-    # result1 = H @ leftup
-    # result2 = H @ leftdown
-    # result3 = H @ rightup
-    # result4 = H @ rightdown
-    # print("result1 is :", result1)
-    # print("result2 is :", result2)
-    # print("result3 is :", result3)
-    # print("result4 is :", result4)
-    """
-
-    image = cv.imread("../../images/image3.png", cv.IMREAD_UNCHANGED)  # 读图, opencv库不指定图像读灰度图的话就算原图是灰度
-    # 图,图像的属性也是三通道的
-    # print(image.shape)
-    N, extend = 16, 16
-
-    # kernel = getMotionDsf(75, 60) # 测试线性去模糊的代码注释,用的是最小二乘滤波
-    #                               # 对应图片blur 60 75.png意思是点移动60像素,方向是75度
-    #                               # 度数以图像坐标系的x轴开始,逆时针为正,顺时针为负
-    # print (kernel.shape[0])
-    # print (kernel.shape[1])
-    # laplacian = np.array([[0, -1, 0], [-1, 4, -1], [0, -1, 0]])
-    # deblur_img = ClsFilter(image, kernel, laplacian, 0.001)
-    # cv.imshow("deblur", deblur_img)
-    # cv.waitKey()
-
-    # image_fft(gray=image, vis=False) # 如果vis为True显示图像的频谱图
-
-    # N: int = 2 # 图像坐标系,图像的X或者Y方向要分几块,如果N=2(2x2),则图像会被分成4等份,N=4,则图像被分为16(4x4)份
-
-    # 相机内参阵主点为(619.0527171517301, 346.8334627145192)
-    # Intrinsics = np.array([[815.5400764083713, 0, 619.0527171517301],
-    #                        [0, 813.4639884474725, 346.8334627145192],
-    #                        [0, 0, 1]])
-
-    # 相机内参阵主点为(619.0527171517301, 346.8334627145192)
-    Intrinsics = np.array([[815.5400764083713, 0, 619.0527171517301],
-                           [0, 813.4639884474725, 346.8334627145192],
-                           [0, 0, 1]])
-
-    # Rotation = np.array([[0.9986, -0.05234, 0],  # 相机旋转, 旋转矩阵 绕z轴旋转3°
-    #                      [0.05234, 0.9986, 0],
-    #                      [0, 0, 1]])
-
-    Rotation = np.array([[0.9961, 0.08716, 0],  # 相机旋转, 旋转矩阵 绕z轴旋转-5°
-                         [-0.08716, 0.9961, 0],
-                         [0, 0, 1]])
-
-    # Rotation = np.array([[1, 0, 0],  # 相机旋转, 旋转矩阵 绕x轴旋转5°
-    #                      [0, 0.9961, 0.08716],
-    #                      [0, -0.08716, 0.9961]])
-
-    # Rotation = np.array([[0.9961, 0, -0.08716],  # 相机旋转, 旋转矩阵 绕y轴旋转5°
-    #                      [0, 1, 0],
-    #                      [0.08716, 0, 0.9961]])
-
-    # Rotation = np.array([[1, 0, 0],  # 相机旋转, 旋转矩阵
-    #                      [0, 1, 0],
-    #                      [0, 0, 1]])
-
-    transion = np.array([0.1, 0, 0])  # 相机位移
-    # transion = np.array([0, 0.05, 0])  # 相机位移
-
-    beg_time = time.time()
-    Homograph = Image.get_homography(Intrinsics, Rotation, transion)  # 计算单应矩阵
-
-    # l, theta = calcu_pixel_motion(Homograph, [0, 0, 1]) # 计算像素点的变化对应的像素位移和角度(u0, v0)->(u1, v1),
-    #                                                     # l是像素位移大小, theta是角度偏移
-    #
-    # print("l is {0}, theta is {1}".format(l, theta))
-    #
-    # kernel = PSF.PSFFunction(l, theta * 180 / np.pi)
-
-    # kernel = get_motion_psf(image.shape, 180 * theta / np.pi, l) # 由l和theta计算线性模糊核
-    # kernel.calculate_h()
-    # blur_image = cv.filter2D(image, -1, kernel.hh) # 图像模糊
-    # cv.imshow("blur", blur_image) #显示图像
-    # cv.waitKey()
-
-    # # # 分割图像成四个等大小的块 #该注释代码已弃用
-    # # # block1 = image[:block_height, :block_width]
-    # # # block2 = image[:block_height, block_width:]
-    # # # block3 = image[block_height:, :block_width]
-    # # # block4 = image[block_height:, block_width:]
-
-    # beg_time = time.time()
-    image_blocks = Image.segment_nimage(image, N, extend, False)  # 按行顺序存放,vis=True则显示分块后的图像
-    # print("expired {0:.12f} seconds".format(time.time() - beg_time))
-    blocks_psf = Image.calcu_each_block_psf(image_blocks=image_blocks, n=N, H=Homograph, vis=True)  # 计算每一块的模糊核,
-    # 存放到blocks_psf(np.array)
-    # 如果vis=False,
-    # 则blocks_psf里面的每一个元素是[l,theta] 如果vis=True,则blocks_psf里面每一个元素都是一个模糊化的图像 for i in range(N): for j in range(N):
-    # flag : bool = cv.imwrite("./block{0}{1}.png".format(i, j), blocks_image[i, j]) print(flag) # print(blocks_psf)
-    # Raw_image = nimage_block_merge(image_blocks=image_blocks, n=N, overlap=extend, vis=False)  # 显示没有模糊化的原图拼接结果
-    blur_image = Image.nimage_block_merge(blocks_psf, N, overlap=extend, vis=False) # 显示模糊化之后的拼接结果,
-    # print(blur_image.shape)
-    # 注意calcu_each_block_psf中的vis=True时才可用
-    print("elapsed {0:.5f} seconds".format(time.time() - beg_time))
+    img0_pub = rospy.Publisher("/left_image", Image, queue_size=10)
+    img1_pub = rospy.Publisher("/right_image", Image, queue_size=10)
+    
+    rospy.spin()
